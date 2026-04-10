67100c47c464146e2fc4c1a4995bfba927e13d0a
priyanshusongara <priyanshusongara23@gmail.com>
2026-04-11 04:17:17 +0530

    adminaccount 5 needs

--- build.sh
+++ build.sh
@@ -2,12 +2,16 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
+
 python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(email="admin@example.com").exists():
+
+if not User.objects.filter(email="admin@frost.com").exists():
     User.objects.create_superuser(
-        "admin@example.com",
+        "Admin",
+        "User",
+        "admin@frost.com",
         "admin",
         "admin123"
     )
