b07ab52d0c9971447b8eaecdbf78a2abace5bbcf
priyanshusongara <priyanshusongara23@gmail.com>
2026-04-11 03:41:46 +0530

    turn debug on

--- frost/settings.py
+++ frost/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-epjp9k3!jh&+l4qt%o8t3m-)sl&dci-j+*pk(ty@j&-b00!sje'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com']
 
