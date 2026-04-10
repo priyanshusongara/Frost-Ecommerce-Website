843eac895899d4c4e956fd632b179de2508df444
priyanshusongara <priyanshusongara23@gmail.com>
2026-04-11 04:12:57 +0530

    admin account custom3

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ python manage.py collectstatic --noinput
 python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "admin123")
+if not User.objects.filter(email="admin@example.com").exists():
+    User.objects.create_superuser(
+        "admin@example.com",
+        "admin",
+        "admin123"
+    )
 EOF
