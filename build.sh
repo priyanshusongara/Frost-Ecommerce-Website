c2958932f51257cb622b728bb18cef617e6ca2db
priyanshusongara <priyanshusongara23@gmail.com>
2026-04-11 04:06:49 +0530

    admin account creation in live

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('Priyanshu','priyanshusongara23@gmail.com','Radha@2002')"
