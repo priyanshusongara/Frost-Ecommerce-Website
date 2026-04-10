2806391f3e727e892c2c45493d4a15c211a62652
priyanshusongara <priyanshusongara23@gmail.com>
2026-04-11 04:10:29 +0530

    admin account creation twice

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('Priyanshu','priyanshusongara23@gmail.com','Radha@2002')"
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "admin123")
+EOF
