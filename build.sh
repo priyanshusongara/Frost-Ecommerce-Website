06dcb38fec7c2e25b977b676d9e541f52ccd2262
priyanshusongara <priyanshusongara23@gmail.com>
2026-04-12 03:49:55 +0530

    cloudinary storage

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py migrate
+
 python manage.py collectstatic --noinput --clear
-python manage.py collectstatic --noinput --clear
+
 python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
 if not User.objects.filter(email="admin@frost.com").exists():
     User.objects.create_superuser(
         "Admin",
         "User",
-        "priyanshusongara23@gmail.com",
         "Priyanshu",
+        "priyanshusongara23@gmail.com",
         "Radha@2002"
     )
 EOF
-
-
