3ef31936ab525429b93dcf2a952ad5dcb44e8a9c
priyanshusongara <priyanshusongara23@gmail.com>
2026-04-11 23:30:58 +0530

    static imagestry 2

--- frost/settings.py
+++ frost/settings.py
@@ -143,14 +143,11 @@ USE_TZ = True
 
 
 import os
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),]
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 
