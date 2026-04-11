407f442d9416841f38712403471aa8a6a8b982f9
priyanshusongara <priyanshusongara23@gmail.com>
2026-04-11 23:54:40 +0530

    static trial 4

--- build.sh
+++ build.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
+
 pip install -r requirements.txt
-python manage.py migrate
+
 python manage.py collectstatic --noinput
-
-python manage.py shell <<EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
-if not User.objects.filter(email="admin@frost.com").exists():
-    User.objects.create_superuser(
-        "Admin",
-        "User",
-        "priyanshusongara23@gmail.com",
-        "Priyanshu",
-        "Radha@2002"
-    )
-EOF
+python manage.py migrate
