622314630a55691889fd055332fca4fe323fa28f
priyanshusongara <priyanshusongara23@gmail.com>
2026-04-12 00:27:53 +0530

    fix static deployment

--- frost/settings.py
+++ frost/settings.py
@@ -144,12 +144,8 @@ USE_TZ = True
 
 import os
 from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
