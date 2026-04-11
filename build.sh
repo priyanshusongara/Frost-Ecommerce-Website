5c7d3ac5a3d63e97328d8a065b2832ef6d0ffed5
priyanshusongara <priyanshusongara23@gmail.com>
2026-04-11 15:53:31 +0530

    mobile version

--- build.sh
+++ build.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
+
 pip install -r requirements.txt
-python manage.py migrate
+
 python manage.py collectstatic --noinput
 
-python manage.py shell <<EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
-if not User.objects.filter(email="admin@frost.com").exists():
-    User.objects.create_superuser(
-        "Admin",
-        "User",
-        "admin@frost.com",
-        "admin",
-        "admin123"
-    )
-EOF
+python manage.py migrate
