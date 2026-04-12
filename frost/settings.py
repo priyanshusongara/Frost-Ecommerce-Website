cccae3fc57a87b0bfc49a65971ea2369e4e87e9d
priyanshusongara <priyanshusongara23@gmail.com>
2026-04-12 14:21:01 +0530

    databases

--- frost/settings.py
+++ frost/settings.py
@@ -88,13 +88,14 @@ TEMPLATES = [
 # =========================
 # DATABASE
 # =========================
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+import os
 
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
+}
 AUTH_USER_MODEL = 'accounts.Account'
 
 # =========================
